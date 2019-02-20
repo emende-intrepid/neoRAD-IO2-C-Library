c9abd0c9cf6bf75ef74895f61181dafa30217ecf
Stan Mende <emende@intrepidcs.com>
2019-02-20 16:05:07 +0800

    Added header for writing out to AOUt

--- inc/neoRAD-IO2-AOUT.h
+++ inc/neoRAD-IO2-AOUT.h
@@ -4,11 +4,17 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-typedef struct _neoRADIO2AOUT_ChannelSetting{
-	uint8_t enable;
-	uint16_t default_value;
-} neoRADIO2AOUT_ChannelSetting;
+	
+typedef union _neoRADIO2AOUT_header {
+	uint8_t byte;
+	struct {
+		unsigned ch1 :1;
+		unsigned ch2 :1;
+		unsigned ch3 :1;
+		unsigned reserved :4;
+		unsigned noCal:1;
+	} bits;
+} neoRADIO2AOUT_header;
 
 #ifdef __cplusplus
 }
